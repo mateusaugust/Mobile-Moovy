cded59c9cc6109a48bef5dd06cd48876d1e1ae4d
mateusaugust <mateusasilva033@gmail.com>
2023-05-23 11:50:07 -0300

    Backup

---
+++ moovy-refactor/src/Cards/CardSearch.js
@@ -0,0 +1,44 @@
+import styles from '../CardsModules/CardSearch.module.css'
+import Button from '@mui/material/Button';
+import { useState } from 'react';
+
+import { BsFillStarFill } from "react-icons/bs";
+
+function CardSearch(props){
+    const apiLinkImg = 'https:/image.tmdb.org/t/p/w500/'
+
+    const[isAdd,setIsAdd]=useState(Boolean(localStorage.getItem(props.id)))
+    
+    const addStorage = () =>{
+
+        let url = 'https://api.themoviedb.org/3/movie/'+props.id+'?api_key=4f29b990a89a1383d4f6c9af65fec3c5&language=en-US'
+        localStorage.setItem(props.id,url)
+       
+        setIsAdd(true)
+    }
+
+    return(
+        <div className={styles.cardGeral}>
+            <div className={styles.image}>
+                <img src = {props.poster_path ? apiLinkImg+props.poster_path : "https://images.unsplash.com/photo-1620177088163-012f511de69d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=385&q=80"} 
+                alt="imagemPosterFilme"/>
+                <div className={styles.overView}>
+                    <h2 className={styles.titleOverview}>Overview:</h2>
+                    <h3 className={styles.overviewInfo}>{props.overview}</h3>
+                </div>
+            </div>
+            <div className={styles.info}>
+                <p>{props.title}</p>
+                <div className={styles.infoDescription}>
+                    <BsFillStarFill className={styles.star}/>
+                    <p className={styles.voter}>{props.vote_average.toFixed(1)}</p>
+                    <Button onClick={addStorage} variant="contained" sx={
+                    isAdd ? {backgroundColor:'#ed9121',height:'2.5rem', width:'6rem',mr:'1.5rem', mt:'0.4rem'} 
+                    : {backgroundColor:'#0ACF83', height:'2.5rem', width:'6rem',mr:'1.5rem', mt:'0.4rem'}} 
+                    >{isAdd ?"Adicionado" : "Adicionar"}</Button>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default CardSearch;
--- moovy-refactor/src/Components/Search.js
+++ moovy-refactor/src/Components/Search.js
@@ -1,8 +1,76 @@
 
-function Search(){
-  return(
-    <h1>Teste2</h1>
-  )
+import { useState} from 'react';
+
+import styles from '../Modules/Search.module.css'
+import CardSearch from '../Cards/CardSearch'
+
+import Paper from '@mui/material/Paper';
+import InputBase from '@mui/material/InputBase';
+import IconButton from '@mui/material/IconButton';
+import SearchIcon from '@mui/icons-material/Search';
+
+
+
+
+
+
+function Search (){
+    
+    const apiSearch = 'https://api.themoviedb.org/3/search/movie?api_key=4f29b990a89a1383d4f6c9af65fec3c5&query='
+
+    const[filmes, setFilmes] = useState([])
+    const[term, setTerm] = useState('')
+   
+    
+  
+
+    const handleSearch = (e) =>{
+       e.preventDefault()
+
+       fetch(apiSearch+term).then(res=>res.json())
+       .then(data=> setFilmes(data.results))
+    }
+
+    return(
+       
+        <section>
+            <div className={styles.divPosition}>
+        
+             
+                    <Paper
+                    component="form"
+                    sx={{ mt:'10px',p: '5px 10px', display: 'flex', alignItems: 'center',
+                    backgroundColor:'#baffd0'
+                    }}
+                    >
+                        <IconButton sx={{ p: '10px' }} aria-label="menu">
+                           
+                        </IconButton>
+                        <InputBase
+                            sx={{ ml: 1, flex: 1 }}
+                            placeholder="Buscar Filme"
+                            inputProps={{ 'Inter': 'aria-label' }}
+                            onChange={(e) => setTerm(e.target.value)}
+                        />
+                        <IconButton  onClick={handleSearch} type="button" sx={{ p: '10px' }} aria-label="search">
+                            <SearchIcon />
+                        </IconButton>
+                        
+                    </Paper>
+               
+
+                <div className={styles.divPesquisa}>
+                {filmes.map((filme) =>{
+                    return(
+                        <CardSearch key={filme.id} {...filme}/>   
+                    )
+                    })
+
+                }   
+                </div>  
+            </div>
+        </section>
+    )
 }
 
 export default Search
